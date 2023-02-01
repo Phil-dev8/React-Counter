11846f6dd51fbecf04afda01d15c8c8d5e08aec4
Philippe Victoria <vp8@mbp-de-philippe.home>
2023-02-01 16:33:41 +0100

    test condition sans succès + problème de CSS

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const App = () => {
             setCounter(counter - 1);
           }}
         >
-          -
+          {counter === 0 && <button></button>}-
         </button>
         <div className="button">
           <p>{counter}</p>
